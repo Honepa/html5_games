ca92d9af3318b4d0ae04426a7858e283624e15d0
Honepa <cot1884@gmail.com>
2021-03-23 23:45:20 +0500

    zachotka

--- Grab_the_Zachotka/zachotka.js
+++ Grab_the_Zachotka/zachotka.js
@@ -1,25 +1,35 @@
 var canvas = document.getElementById('canvas');
 var ctx    = canvas.getContext("2d");
 
-var x = canvas.width/2;
+var score = 0;
+var speed = 2;
+var x = getRandomX(0, canvas.width - 10);
 var y = canvas.height/2;
 var leftPress = false;
 var rightPress = false;
 x_zach = x - 100;
 y_zach = canvas.height - 50;
-var dx = x_zach
+var dx = canvas.width/2 - 100;
+var y_el = 0;
+
+function getRandomX(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min; //Максимум не включается, минимум включается
+}
+
 function keyUpHandler(e){
     if(e.key == "Right" || e.key == "ArrowRight")
     {
         rightPress = false;
 
-        console.log(dx);
+        //console.log(dx);
     }
     else if(e.key == "Left" || e.key == "ArrowLeft") {
 
       leftPress = false;
 
-        console.log(dx);
+        //console.log(dx);
     }
 
 }
@@ -46,12 +56,24 @@ function draw_zachotka() {
 
 }
 
-document.addEventListener("keydown", keyDownHandler, false);
-document.addEventListener("keyup", keyUpHandler, false);
+function draw_element()
+{
+  ctx.beginPath();
+  ctx.rect(x, y_el, 10, 10);
+  ctx.fillStyle = "#0095DD";
+  ctx.fill();
+  ctx.closePath();
+  //console.log("element");
+}
 
-function draw() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  draw_zachotka();
+function drawScore() {
+    ctx.font = "16px 'Pixel'";
+    ctx.fillStyle = "#0095DD";
+    ctx.fillText("Очки: "+score, 8, 20);
+}
+
+function move_racket()
+{
   if(leftPress)
   {
     dx -= 10;
@@ -67,6 +89,61 @@ function draw() {
   if(dx <= 0)
   {
     dx = 0;
+  }
+}
+
+function chek_element() {
+  if(y_el >= 455)
+  {
+    if((x >= dx) & (x <= dx + 200))
+    {
+      //console.log("new_ochko");
+      score++;
+    }
+    else{
+      //console.log("no_ochko");
+      score--;
+    }
+    y_el = 0;
+    x = getRandomX(0, canvas.width - 10);
+  }
+  else {
+    y_el += speed;
+  }
+}
+
+document.addEventListener("keydown", keyDownHandler, false);
+document.addEventListener("keyup", keyUpHandler, false);
+
+function draw() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  draw_element();
+  draw_zachotka();
+  move_racket();
+  chek_element();
+  drawScore();
+  if(score >= 5)
+  {
+    //alert("end game!");
+    speed = 5;
+    if(score > 10)
+    {
+      speed = 7;
+      if (score > 15) {
+        speed = 10;
+          if(score > 20)
+          {
+            speed = 12;
+          }
+      }
+    }
+
+  }
+  if(score < 0)
+  {
+    alert("end game(");
+    score = 0;
+    window.location.reload();
   }
 }
 
